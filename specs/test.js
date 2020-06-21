81c20e8d437cdf1f7387b26b6675d71f47c02a4d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-20 19:05:16 -0700

    Add dummy Jest test of a React component to ensure set up for .jsx files

--- specs/test.js
+++ specs/test.js
@@ -1,3 +1,5 @@
+import App from '../client/src/components/App.jsx';
+
 const { findForId, updateReview, updateImage } = require('../server/routeHandlers.js');
 
 test('findForId is a function', () => {
@@ -10,4 +12,8 @@ test('updateReview is a function', () => {
 
 test('updateImage is a function', () => {
   expect(typeof updateImage).toBe('function');
+});
+
+test('App is a function', () => {
+  expect(typeof App).toBe('function');
 });
