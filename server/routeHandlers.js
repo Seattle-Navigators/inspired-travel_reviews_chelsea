ddedc87420e63d04b544d63c563f1213f7e057a7
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-21 15:00:53 -0700

    Added three unit tests for server route handler functions

--- server/routeHandlers.js
+++ server/routeHandlers.js
@@ -1,37 +1,42 @@
 const Review = require('../database/Reviews.js');
 
-module.exports.findForId = (req, res) => {
+module.exports.findForId = (req, res, testCb = () => {}) => {
   Review.find({ attractionId: req.params.productId }, (err, docs) => {
     if (err) {
       res.sendStatus(500);
+      testCb(err);
     } else {
       res.status(200);
       res.json(docs);
+      testCb(null, docs);
     }
   });
 };
 
-module.exports.updateReview = (req, res) => {
-  Review.updateOne({ _id: req.params.reviewId }, { helpful: true }, (err) => {
+module.exports.updateReview = (req, res, testCb = () => {}) => {
+  Review.updateOne({ _id: req.params.reviewId }, { helpful: true }, (err, data) => {
     if (err) {
       res.sendStatus(500);
+      testCb(err);
     } else {
-      console.log('reviewpatch');
       res.sendStatus(200);
+      testCb(null, data);
     }
   });
 };
 
-module.exports.updateImage = (req, res) => {
+module.exports.updateImage = (req, res, testCb = () => {}) => {
   Review.collection.updateOne({
     'uploadImages.id': req.params.imageId,
   }, {
     $set: { 'uploadImages.$.helpful': true },
   })
-    .then(() => {
+    .then((data) => {
       res.sendStatus(200);
+      testCb(null, data);
     })
-    .catch(() => {
+    .catch((err) => {
       res.sendStatus(500);
+      testCb(err);
     });
 };
--- specs/test.js
+++ specs/test.js
@@ -1,30 +1,97 @@
 import App from '../client/src/components/App.jsx';
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-
 const { findForId, updateReview, updateImage } = require('../server/routeHandlers.js');
-// const casper = require('casper').create();
+const Review = require('../database/Reviews.js');
 
-// test('CasperJS', () => {
-//   casper.open('http://localhost:3004');
-//   casper.then(function() {
-//     this.echo(this.getTitle());
-//     expect(true).toBe(true);
-//   });
-// });
+const attractionId = '200'; // okay to use any id above 100 for testing
+const mockRes = {
+  status: () => {},
+  json: () => {},
+  send: () => {},
+  sendStatus: () => {},
+};
+let reviewId;
+let imageId;
 
-test('findForId is a function', () => {
-  expect(typeof findForId).toBe('function');
+beforeAll(() => {
+  const testReviews = [];
+  for (let i = 0; i < 5; i++) {
+    testReviews.push({
+      user: {
+        originCountry: 'United States',
+        originRegion: 'Oregon',
+        contributions: 999,
+        name: 'Joe Bob',
+        profileImage: 'https://fec-images-6-18-20.s3-us-west-2.amazonaws.com/profiles/img1.jpg',
+      },
+      uploadImages: [
+        {
+          id: `${attractionId}1${i}`,
+          helpful: false,
+          url: 'https://fec-images-6-18-20.s3-us-west-2.amazonaws.com/userUploads/img19.jpg',
+          username: 'joeB',
+          createdAt: new Date(),
+          reviewTitle: 'What a great place this was',
+          reviewRating: 3,
+        },
+      ],
+      attractionId,
+      rating: 3,
+      travelType: 'Solo',
+      expDate: new Date(),
+      lang: 'English',
+      body: 'Like I said, a great place',
+      title: 'What a great place this was',
+      votes: 999,
+      createdAt: new Date(),
+      helpful: false,
+    });
+  }
+  return Review.create(testReviews)
+    .then(() => {
+      Review.find({ attractionId }, (err, docs) => {
+        reviewId = docs[0]._id;
+        imageId = docs[0].uploadImages[0].get('id');
+        return;
+      });
+    });
 });
 
-test('updateReview is a function', () => {
-  expect(typeof updateReview).toBe('function');
+afterAll(() => {
+  return Review.deleteMany({ attractionId })
+    .then(() => {
+      return;
+    });
 });
 
-test('updateImage is a function', () => {
-  expect(typeof updateImage).toBe('function');
+test('findForId finds all reviews for a given attraction ID', (done) => {
+  findForId({ params: { productId: attractionId } }, mockRes, (err, docs) => {
+    expect(err).toBe(null);
+    expect(docs.length).toEqual(5);
+    done();
+  });
 });
 
-test('App is a function', () => {
-  expect(typeof App).toBe('function');
+test('updateReview changes a single review\'s "helpful" property to true', (done) => {
+  updateReview({ params: { reviewId } }, mockRes, (err, data) => {
+    expect(err).toBe(null);
+    Review.findOne({ _id: reviewId }, (err, doc) => {
+      expect(doc.helpful).toBe(true);
+      done();
+    });
+  });
+});
+
+test('updateImage changes a single image\'s "helpful" property to true', (done) => {
+  updateImage({ params: { imageId } }, mockRes, (err, data) => {
+    expect(err).toBe(null);
+    Review.findOne({ 'uploadImages.id': imageId }, (err, doc) => {
+      expect(doc.uploadImages[0].get('helpful')).toBe(true);
+      done();
+    });
+  });
+});
+
+test('App is a function', (done) => {
+  expect(typeof App).toEqual('function');
+  done();
 });
