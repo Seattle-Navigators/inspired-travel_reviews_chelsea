56b5bd80d570132d96e2aea22a1ac73d6c8e46e5
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-23 21:08:36 -0700

    Fix linting errors

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 const getAttractionId = (ref) => {
-    const url = new URL(ref);
-    const path = url.pathname;
-    const idSearch = /(\d{3})/;
-    const [attractionId] = path.match(idSearch);
-    return attractionId;
+  const url = new URL(ref);
+  const path = url.pathname;
+  const idSearch = /(\d{3})/;
+  const [attractionId] = path.match(idSearch);
+  return attractionId;
 };
 
 const attractionId = getAttractionId(window.location.href);
