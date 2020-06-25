12d4a926e95b2aab81a746be25c4939a832f7f21
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-25 11:24:58 -0700

    Add X button to exit expanded languages view

--- client/src/components/Languages.jsx
+++ client/src/components/Languages.jsx
@@ -17,6 +17,7 @@ const Languages = ({ hidden, handleViewSwitch, langs }) => {
     <div>
       <div className="popup-lang-background" id="exit-lang-popup" onClick={handleViewSwitch} role="button" aria-label="Exit popup" onKeyDown={handleViewSwitch} tabIndex={-1} />
       <div className="popup-lang">
+        <button onClick={handleViewSwitch}>X</button>
         {langs.map((lang) => <ListItem value={lang[0]} records={lang[1]} type="radio" key={`${lang}-label`} name="expand-langs" />)}
       </div>
     </div>
