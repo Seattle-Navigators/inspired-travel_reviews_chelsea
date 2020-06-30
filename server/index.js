74ad2a5e08fe3d4b817c3539eb8aac791d0b352c
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-29 20:38:57 -0700

    Complete steps to make integration with proxy server possible

--- server/index.js
+++ server/index.js
@@ -7,12 +7,6 @@ const { findForId, updateReview, updateImage } = require('./routeHandlers.js');
 
 const port = 3004;
 const publicDir = path.resolve(__dirname, '..', 'client', 'public');
-const publicBundle = path.resolve(__dirname, '..', 'client', 'public', 'bundle.js');
-const options = {
-  setHeaders: (res) => {
-    res.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-  },
-};
 
 const app = express();
 
@@ -20,27 +14,8 @@ app.use(cors({origin: 'http://127.0.0.1:3000'}));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-// app.set('trust proxy', 'loopback');
+app.use('/:productId/reviewsModule', express.static(publicDir));
 
-// app.use((req, res, next) => {
-//   res.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-//   res.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-//   res.set('Access-Control-Allow-Headers', '*');
-//   next();
-// });
-// app.use('/:productId/', express.static(publicDir, options));
-app.use('/:productId/test', express.static(publicDir));
-// app.use('/:productId/', express.static(publicBundle, options));
-// app.get('/:productId/', (req, res) => {
-//   res.status(200);
-//   res.sendFile(publicBundle);
-// });
-
-// app.get('/:productId/test2', (req, res) => {
-//   res.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-//   res.status(200);
-//   res.sendFile(publicBundle);
-// });
 app.get('/:productId/api/reviews', findForId);
 app.patch('/:productId/api/reviews/:reviewId', updateReview);
 app.patch('/:productId/api/reviews/:reviewId/:imageId', updateImage);
